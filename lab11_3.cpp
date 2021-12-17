d938f7b353545bf3da42581479e2ed88ce3afc9c
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-17 16:43:11 +0700

    commit  lap11_3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,38 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source("score.txt");
+    double c , A , sum , sumA , mean ;
+
+    string tt ;
+    while (getline(source , tt) )
+    {
+        sum += atof(tt.c_str()) ;
+        c++ ;
+    }
+    mean = sum/c ;
+
+    source.close() ;
+    
+    source.open("score.txt");
+    while (getline(source , tt) )
+    {
+        sumA += pow( (atof(tt.c_str())-mean) , 2 )  ;
+    }
+    A = sqrt(sumA/c) ;
+    
+    cout << "Number of data = " << c << '\n' ;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n' ;
+    cout << "Standard deviation = " << A ;
+
+}
+
+
+
+
